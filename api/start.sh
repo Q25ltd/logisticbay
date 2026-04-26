69ba0fcc2024bf15b643bf6348eb2664b589ae61
Q25ltd <q25limited@gmail.com>
2026-04-26 19:38:42 +0100

    fix: pre-deploy handles db push, start.sh just runs server

--- api/start.sh
+++ api/start.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-set -e
-echo "DATABASE_URL is set: $(echo $DATABASE_URL | cut -c1-30)..."
-echo "Running prisma db push..."
-npx prisma db push --schema=./prisma/schema.prisma --accept-data-loss
-echo "Prisma push done!"
-echo "Starting server..."
 exec npx tsx src/server.ts
